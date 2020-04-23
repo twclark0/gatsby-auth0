87a6203a89b3fe9fb0867b08a4d0fa02c69f7c6e
twclark0 <tylerc644@gmail.com>
2020-04-23 14:01:42 -0600

    initial commit

---
+++ src/components/bio.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { rhythm } from "../utils/typography";
+
+const Bio = () => {
+  return (
+    <div
+      style={{
+        display: `flex`,
+        marginBottom: rhythm(2.5),
+      }}
+    ></div>
+  );
+};
+
+export default Bio;
