0c6f1c9351f560c9a9d8972b113e7f2a969a8a6a
twclark0 <tylerc644@gmail.com>
2020-04-24 11:26:13 -0600

    Create nav-bar component

---
+++ src/components/nav-bar/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export const Navigation = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/account">My Account</Link>
+  </nav>
+)
